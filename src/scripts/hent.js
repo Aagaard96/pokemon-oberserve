69e246df1370ea02e480534f444701ea2607b4d6
Andreas <u2455783@rts-365.dk>
2023-10-24 13:54:45 +0200

    Localstorage - API - Observe

---
+++ src/scripts/hent.js
@@ -0,0 +1,15 @@
+export default (async function() {
+	const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+	const pokemon = await response.json()
+
+    localStorage.setItem("pokemon", JSON.stringify(pokemon))
+const savedPokemon = JSON.parse(localStorage.getItem("pokemon"))
+
+console.log(pokemon)
+
+document.querySelector(".hent").innerHTML = `
+<h1>${savedPokemon.name}<h1>
+<h2>Weight:${savedPokemon.weight}<h2>
+<h3>${savedPokemon.abilities[0].ability.name}
+`
+})()
